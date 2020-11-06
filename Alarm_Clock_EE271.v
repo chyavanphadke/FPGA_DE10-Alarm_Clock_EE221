dbba42f9ceacfd6ac75f24ae702b581a77563441
chyavanphadke <chyavanphadke@gmail.com>
2020-11-05 18:06:19 -0800

    State machine generated

--- Alarm_Clock_EE271.v
+++ Alarm_Clock_EE271.v
@@ -1,19 +1,12 @@
-
 module Alarm_Clock_EE271 (
-  seg0, seg1, seg2, seg3, seg4, seg5, 
-  switches, pushBtn, led,
-  clk
+	clk, 
+	pushBtn, 
+	switches, 
+	led, 
+	seg0, seg1, 
+	seg2, seg3, 
+	seg4, seg5
 );
-
-// Initilisations goes from here >>>>>>>>>>>>>>>>>>>>>>>
-
-// Seven Segment Display
-output reg [7:0] seg0;
-output reg [7:0] seg1;
-output reg [7:0] seg2;
-output reg [7:0] seg3;
-output reg [7:0] seg4;
-output reg [7:0] seg5;
 
 // Switches
 input [9:0] switches;
@@ -21,17 +14,30 @@ input [9:0] switches;
 // Push button
 input [1:0] pushBtn;
 
-// LED's
-input [9:0] led;
-
 // Clock
 input clk;
 
-parameter resetState = 0, alarmState = 1, timerState = 2, stopwatchState = 3;
+// LED's
+output [9:0] led;
 
-// Code goes from here >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 
-reg [2:0] currentState, nextState;
+output reg [7:0] seg0;
+output reg [7:0] seg1;
+output reg [7:0] seg2;
+output reg [7:0] seg3;
+output reg [7:0] seg4;
+output reg [7:0] seg5;
+
+reg [1:0] currentState;
+
+reg [24:0]counter_pressed, counter_not_pressed;
+reg button_state = 1'b1;
+
+initial begin
+currentState <= 2'b0;
+counter_pressed <= 25'b0;
+counter_not_pressed <= 25'b0;
+end
 
 initial begin
   seg0 = 8'b11_11_11_11;
@@ -40,25 +46,70 @@ initial begin
   seg3 = 8'b11_11_11_11;
   seg4 = 8'b11_11_11_11;
   seg5 = 8'b11_11_11_11;
-  currentState = 0 ;
 end
 
-always @ (posedge pushBtn) begin
-  if (!pushBtn[0])
-    currentState = currentState + 1;
-    if (currentState > 3) begin
-      currentState = 0;
-  end
+always @ (posedge clk or negedge pushBtn[1])
+begin
+	if(!pushBtn[1])
+		begin
+		currentState <= 2'b0;
+		counter_pressed<= 25'b0;
+		counter_not_pressed<= 25'b0;
+		end
+	
+	else
+		begin
+	
+		if(!pushBtn[0] & !button_state)
+			begin
+			counter_pressed <= counter_pressed + 1'b1;
+			end
+
+		else
+			begin
+			counter_pressed <= 25'b0;
+			end
+
+		if(counter_pressed == 25'd2000000)
+			begin
+			currentState = currentState + 1'b1;
+			counter_pressed <= 25'b0;
+			button_state = 1'b1;
+			end
+
+		if(pushBtn[0] & button_state)
+			begin
+			counter_not_pressed <= counter_not_pressed + 1'b1;		
+			end
+
+		else
+			begin
+			counter_not_pressed <= 25'b0;
+			end
+
+		if(counter_not_pressed == 25'd2000000)
+			begin
+			counter_not_pressed <= 25'b0;
+			button_state = 1'b0;
+			end
+		end
 end
 
+assign led[7:0] = currentState[1:0];
 
 always @ (currentState) begin
   case(currentState)
-    2'b00 : seg0 = 8'b11_00_00_00;
-    2'b01 : seg0 = 8'b11_11_10_01;
-    2'b10 : seg0 = 8'b10_10_01_00;
-    2'b11 : seg0 = 8'b10_11_00_00;
+		0: seg0 = 8'b11_00_00_00;
+		1: seg0 = 8'b11_11_10_01;
+		2: seg0 = 8'b10_10_01_00;
+		3: seg0 = 8'b10_11_00_00;
+		4: seg0 = 8'b10_01_10_01;
+		5: seg0 = 8'b10_01_00_10;
+		6: seg0 = 8'b10_00_00_10;
+		7: seg0 = 8'b11_11_10_00;
+		8: seg0 = 8'b10_00_00_00;
+		9: seg0 = 8'b10_01_00_00;
   endcase
 end
 
-endmodule
+endmodule 
